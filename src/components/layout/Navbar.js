1886de366811e760cdce6cc6b843b62a629038b7
ahmedzaki98 <ahmedzaki19998@gmail.com>
2022-12-13 15:01:46 +0200

    Added counter to cart icon

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import logo from "./Logo.png";
 import styled from "styled-components";
 import { ButtonContainer } from "../ui/Button";
+import { ProductConsumer } from "../../context";
 
 const NavWrapper = styled.nav`
   background: var(--mainBlue);
@@ -16,29 +17,59 @@ const NavWrapper = styled.nav`
     hight: 5.5rem;
   }
 `;
+const SpanWrapper = styled.span`
+  i {
+    max-width:0.9rem;
+    margin-left: -1rem;
+    margin-right: 1rem;
+    color: var(--lightBlue);
+    font-size: 1.3rem;
+  }
+  .badge:after {
+    content: attr(value);
+    font-size: 0.7rem;
+    color: #fff;
+    background: var(--mainBlue);
+    border-radius: 50%;
+    padding: 2px 5px;
+    position: relative;
+    left: -8px;
+    top: -10px;
+    opacity: 0.9;
+  }
+`;
 
 const Navbar = () => {
   return (
-    <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
-      <Link to="/">
-        <img src={logo} alt="store" className="navbar-brand"></img>
-      </Link>
-      <ul className="navbar-nav align-items-center">
-        <li className="nav-item ml-5">
-          <Link to="/" className="nav-link">
-            products
-          </Link>
-        </li>
-      </ul>
-      <Link to="/cart" style={{ marginLeft: "auto" }}>
-        <ButtonContainer>
-          <span className="mr-2">
-            <i className="fas fa-cart-plus"></i>
-          </span>
-          <span>my cart</span>
-        </ButtonContainer>
-      </Link>
-    </NavWrapper>
+    <ProductConsumer>
+      {(value) => {
+        const counter = value.cart.length;
+        return (
+          <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            <Link to="/">
+              <img src={logo} alt="store" className="navbar-brand"></img>
+            </Link>
+            <ul className="navbar-nav align-items-center">
+              <li className="nav-item ml-5">
+                <Link to="/" className="nav-link">
+                  products
+                </Link>
+              </li>
+            </ul>
+            <Link to="/cart" style={{ marginLeft: "auto" }}>
+              <ButtonContainer>
+                <SpanWrapper>
+                  <span><i class="fa badge fa-lg" value={counter}>
+                    &#xf07a;
+                  </i></span>
+                </SpanWrapper>
+                <span> My cart</span>
+              </ButtonContainer>
+            </Link>
+          </NavWrapper>
+        );
+      }}
+    </ProductConsumer>
   );
 };
 export default Navbar;
